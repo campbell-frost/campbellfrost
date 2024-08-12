7188f9523918d2880025531b5c435c074c1fb2a6
campbell-frost <campbellsfrost@gmail.com>
2024-08-11 23:55:40 -0400

    Added Hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,21 +1,36 @@
+import Image from "next/image";
 import { Highlight } from "./ui/hero-background";
 
-/* eslint-disable react/no-unescaped-entities */
 const Hero = () => {
   return (
     <div className="grid grid-cols-2">
       <div className="flex justify-start">
-        {" "}
         <div>
           <p className="text-xl font-bold">Hi, I'm Campbell</p>
-          <p className=" text-6xl mt-6 font-bold mb-10 leading-tight">
+          <p className="text-6xl mt-6 font-bold mb-10 leading-tight">
             I am a full stack <Highlight>software engineer</Highlight> with a
             passion for crafting digital solutions
           </p>
+          <p className="text-xl">
+            Contact me -{" "}
+            <a href="mailto:campbellsfrost@gmail.com" className="email-link">
+              campbellsfrost@gmail.com
+            </a>
+          </p>
         </div>
       </div>
-      <div className="flex justify-end">bye</div>
+      <div className="flex justify-end">
+        <div className="relative w-full aspect-square">
+          <Image
+            src="/images/me_rock.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="thumbnail"
+          />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Hero;
