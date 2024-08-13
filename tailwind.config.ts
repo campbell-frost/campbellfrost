e90f2d9bc7d93372d8dfe8ed02cef68fb836e62a
campbell-frost <campbellsfrost@gmail.com>
2024-08-12 22:55:18 -0400

    Updated tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss"
 import svgToDataUri from "mini-svg-data-uri";
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ["class"],
