d5f1350169fa47c7d585fd1b5220d68b3ede8337
campbell-frost <campbellsfrost@gmail.com>
2025-01-28 18:37:42 -0500

    mobile friendly

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import { Contact } from "@/components/Contact";
 
 export default function Home() {
   return (
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,32 +1,37 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader } from "./ui/card"
-import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel"
-import Autoplay from "embla-carousel-autoplay"
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "./ui/card";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 type ProjectProps = {
-  project: ProjectData,
-}
+  project: ProjectData;
+};
 
 type ProjectData = {
-  name: string,
-  company: string,
-  imageUrl: string,
-  description: string,
+  name: string;
+  company: string;
+  imageUrl: string;
+  description: string;
   technologies: {
-    imageUrl: string,
-    alt: string,
-  }[],
-}
+    imageUrl: string;
+    alt: string;
+  }[];
+};
 
 const Project = ({ project }: ProjectProps) => {
   return (
     <Card>
-      <CardHeader>{project.name}
-        <CardDescription>
-          {project.company}
-        </CardDescription>
+      <CardHeader>
+        {project.name}
+        <CardDescription>{project.company}</CardDescription>
       </CardHeader>
       <CardContent>
         <div className="flex justify-center items-center">
@@ -38,15 +43,19 @@ const Project = ({ project }: ProjectProps) => {
             className="rounded-sm"
           />
         </div>
-        <div className="pt-6">
-          {project.description}
-        </div>
+        <div className="pt-6">{project.description}</div>
       </CardContent>
       <CardFooter className="flex-wrap">
         {project.technologies.map((tech) => (
           <div className="pr-3 pb-3" key={tech.imageUrl}>
             <div className="flex border rounded-lg items-center">
-              <Image className="m-3 rounded-sm" src={tech.imageUrl} alt={tech.alt} width={40} height={40} />
+              <Image
+                className="m-3 rounded-sm"
+                src={tech.imageUrl}
+                alt={tech.alt}
+                width={40}
+                height={40}
+              />
               <p className="mx-auto block mr-4">{tech.alt}</p>
             </div>
           </div>
@@ -54,7 +63,7 @@ const Project = ({ project }: ProjectProps) => {
       </CardFooter>
     </Card>
   );
-}
+};
 
 const projectUrl = "/images/projectImages/";
 const techUrl = "/images/technologyImages/";
@@ -64,7 +73,8 @@ const projects: ProjectData[] = [
     name: "Supacrud",
     company: "Personal Project",
     imageUrl: `${projectUrl}Supacrud.png`,
-    description: "A CLI tool written in TypeScript with the oclif CLI library designed to generate CRUD operations for TypeScript projects using Supabase. This tool streamlines development by automating repetitive database tasks, making integration with Supabase seamless for developers.",
+    description:
+      "A CLI tool written in TypeScript with the oclif CLI library designed to generate CRUD operations for TypeScript projects using Supabase. This tool streamlines development by automating repetitive database tasks, making integration with Supabase seamless for developers.",
     technologies: [
       {
         imageUrl: `${techUrl}TypescriptLogo.png`,
@@ -78,31 +88,32 @@ const projects: ProjectData[] = [
         imageUrl: `${techUrl}OclifLogo.png`,
         alt: "oclif",
       },
-    ]
+    ],
   },
   {
     name: "Trip Tracker",
     company: "Personal Project",
-    description: "A full-stack web application built with Next.js and TypeScript that enables users to document and manage their travel experiences. The platform features a modern, responsive interface styled with Tailwind CSS and integrates with Supabase for secure user authentication and real-time data storage.",
+    description:
+      "A full-stack web application built with Next.js and TypeScript that enables users to document and manage their travel experiences. The platform features a modern, responsive interface styled with Tailwind CSS and integrates with Supabase for secure user authentication and real-time data storage.",
     imageUrl: `${projectUrl}TripTracker.png`,
     technologies: [
       {
         imageUrl: `${techUrl}NextJsLogo.png`,
-        alt: "NextJs"
+        alt: "NextJs",
       },
       {
         imageUrl: `${techUrl}TypescriptLogo.png`,
-        alt: "TypeScript"
+        alt: "TypeScript",
       },
       {
         imageUrl: `${techUrl}SupabaseLogo.png`,
-        alt: "Supabase"
+        alt: "Supabase",
       },
       {
         imageUrl: `${techUrl}TailwindLogo.png`,
         alt: "Tailwind",
-      }
-    ]
+      },
+    ],
   },
   {
     name: "Sign Me In",
@@ -126,7 +137,7 @@ const projects: ProjectData[] = [
       {
         imageUrl: `${techUrl}EntityFrameworkLogo.png`,
         alt: "EF Core",
-      }
+      },
     ],
   },
   {
@@ -138,11 +149,11 @@ const projects: ProjectData[] = [
     technologies: [
       {
         imageUrl: `${techUrl}CSharpLogo.png`,
-        alt: "C#"
+        alt: "C#",
       },
       {
         imageUrl: `${techUrl}PostgresLogo.png`,
-        alt: "Postgres"
+        alt: "Postgres",
       },
       {
         imageUrl: `${techUrl}MVCLogo.png`,
@@ -155,23 +166,23 @@ const projects: ProjectData[] = [
     company: "Francis Marion University",
     imageUrl: `${projectUrl}BossyBooks.png`,
     description:
-      "A full stack web app made as my second Capstone designed to help small business owners track customers and any invoices those customers have, as well as a calander to track upcoming events..  The app is complete with both Google OAuth and standard email auth.",
+      "A full stack web app made as my second Capstone designed to help small business owners track customers and any invoices those customers have, as well as a calendar to track upcoming events.  The app is complete with both Google OAuth and standard email auth.",
     technologies: [
       {
         imageUrl: `${techUrl}NextJsLogo.png`,
-        alt: "Next"
+        alt: "Next",
       },
       {
         imageUrl: `${techUrl}TypescriptLogo.png`,
-        alt: "Typescript"
+        alt: "Typescript",
       },
       {
         imageUrl: `${techUrl}ReactLogo.png`,
-        alt: "React"
+        alt: "React",
       },
       {
         imageUrl: `${techUrl}PostgresLogo.png`,
-        alt: "Postgres"
+        alt: "Postgres",
       },
     ],
   },
@@ -179,31 +190,36 @@ const projects: ProjectData[] = [
 
 const Projects = () => {
   return (
-    <div className="relative max-w-7xl mx-auto py-10 px-6">
+    <div className="relative max-w-7xl py-10 px-5 mx-auto">
       <div className="flex flex-wrap items-center justify-between">
         <h1 className="text-3xl mb-10 font-bold">Projects I&apos;ve built </h1>
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          plugins={[
-            Autoplay({
-              delay: 5000,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            {projects.map((project, index) => (
-              <CarouselItem key={index} className="basis-1/2 md:basis-1/2 lg:basis-1/3 sm:basis-1/2">
-                <Project project={project}></Project>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel >
+        <div className="w-full overflow-hidden px-1">
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            plugins={[
+              Autoplay({
+                delay: 500000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {projects.map((project, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-full sm:basis-1/2 md:basis-1/2 lg:basis-1/3"
+                >
+                  <Project project={project}></Project>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
